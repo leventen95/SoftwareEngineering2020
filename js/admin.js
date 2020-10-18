53db247b37fa1f6a78c3bf6c920f2e7daeda1c96
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2020-10-18 08:50:47 +0200

    Fikset testing av new parking hous
    Dødde samttidg litt på inssiden når jeg innså etter 2 timer og natt uten søvn at det parking list blir og forblir forandret i varriabel verdi etter hver testing...

--- js/admin.js
+++ js/admin.js
@@ -9,10 +9,10 @@ window.onload = startup;
 
 //Currrent format is [] = adress, city, longititude, lattitude
 var parkingHouseList = [
-    ["BRA VEIEN 6a", "Halden", 45.32, 32131],
-    ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 32131],
+    ["BRA VEIEN 6a", "Halden", 45.32, 321.31],
+    ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 321.31],
     ["Ant 23", "Moss", 423.231, 62.132]
-];
+]
 
 
 var xmlhttp;
@@ -22,17 +22,17 @@ function startup() {
     xmlhttp.onreadystatechange = readFile;
     xmlhttp.open("GET", "../parkingHouses.csv", true);
     xmlhttp.send();
-
-
+    
+    
     add = document.getElementById("addButton");
     remove = document.getElementById("removeButton");
-
-
+    
+    
     add.addEventListener("click", addParkingHouse);
     remove.addEventListener("click", removeParkingHouse);
-
-
-
+    
+    
+    
 }
 
 
@@ -40,62 +40,63 @@ function startup() {
 function readFile() {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
         let response = xmlhttp.responseText;
-
+        
         parkingHouseList = response.result.split("\n");
-
-
+        
+        
         for (let i = 0; i < parkingHouseList.length; i++) {
             //Splits the parking house into a 2d array on comma
             parkingHouseList[i] = parkingHouseList[i].split(",")
         }
-
-
+        
+        
         console.log("Loadead parking HouseList complete: \n " + parkingHouseList);
-
+        
     }
-
+    
 }
 
 
-function addParkingHouse([newAdress, newCity, newLatitude, newLongtitude)
-    //newAdress, newCity, newLatitude, newLongtitude
-     {
-        /*
+function addParkingHouse(newAdress, newCity, newLatitude, newLongtitude)
+//newAdress, newCity, newLatitude, newLongtitude
+{
+    /*
     let newAdress = prompt("Please enter the new parkings house's  Adress");
     let newCity = prompt("Please enter the new parkings house's  City");
     let newLongtitude = prompt("Please enter the new parkings house's Longtitude");
     let newLatitude = prompt("Please enter the new parkings house's Latitude");
-*/
-    let newParkingHouseCheck = [newAdress, newCity, newLatitude, newLongtitude];
-
-    console.log("OLD! " + parkingHouseList)
-
-    
-    parkingHouseList.push(newParkingHouse);
-
-    // writeToCsv();
-
-    
-    console.log("NEW! :" +parkingHouseList)
-    return parkingHouseList;
-
+    */
+   let newParkingHouseCheck = [newAdress, newCity, newLatitude, newLongtitude];
+   
+   console.log("OLD! " + parkingHouseList)
+   
+   
+   //parkingHouseList[parkingHouseList.length]=newParkingHouseCheck;
+   parkingHouseList.splice(parkingHouseList.length, 0, newParkingHouseCheck)
+   
+   // writeToCsv();
+   
+   
+   console.log("NEW! :" +parkingHouseList)
+   return parkingHouseList;
+   
 }
 
 
 function writeToCsv() {
     let csvRows = [];
-
-
-
+    
+    
+    
     for (let i = 0; i < parkingHouseList.length; i++) {
         csvRows[i] = parkingHouseList[i].valueOf();
     }
-
+    
     let dataToCsv = csvRows[0];
-
+    
     for (let i = 1; i < csvRows.length; i++) {
-
-
+        
+        
         //adds a new line as long as it isnt last one
         if (i === csvRows.length - 1) {
             dataToCsv = dataToCsv + "\n" + csvRows[i];
@@ -112,69 +113,71 @@ function writeToCsv() {
 function removeParkingHouse(removeTargetAdress, removeTargetCity) {
     //removeTargetAdress = document.getElementById("removeAdress").value;
     //removeTargetCity = document.getElementById("removeInCity").value;
-   
+    
     for (let i = 0; i < parkingHouseList.length; i++) {
-
-
+        
+        
         if (removeTargetAdress === parkingHouseList[i][0]
             && removeTargetCity === parkingHouseList[i][1]) {
-          
-
-           //Honesthly no idea but it works. I kinda know but confused 
-        cloneArray = parkingHouseList.slice();
-
-        parkingHouseList.splice(i, 1);
-
-          
-            
-            //removes the extra arry created
                 
+                
+                //Honesthly no idea but it works. I kinda know but confused 
+                let cloneArray = parkingHouseList.slice();
+                
+                parkingHouseList.splice(i, 1);
+                
+                
+                
+                //removes the extra arry created
+                
+                
+                //writeToCsv();
+                
+                return parkingHouseList;
+                
+            }
             
-            //writeToCsv();
-            
-            return parkingHouseList;
-
+            /*else if (i = parkingHouseList.length - i) {
+                alert("No parking house on that adress");
+            }*/
         }
-
-        /*else if (i = parkingHouseList.length - i) {
-            alert("No parking house on that adress");
-        }*/
     }
-}
-
-
-
-
-function findPlace(targetAdress, targetCity) {
-
-    for (let i = 0; i < parkingHouseList.length; i++) {
-
-
-        if (targetAdress === parkingHouseList[i][0]
-            && targetCity === parkingHouseList[i][1]) {
-
-            return true;
-
-        }
-
-        
-    }
-}
-
-function returnArray() {
-    return parkingHouseList;
-}
-
-function newArray(newAdress, newCity, newLatitude, newLongtitude){
-    let newParkingHouseCheck = [newAdress, newCity, newLatitude, newLongtitude];
-    return newParkingHouseCheck
-}
-
-module.exports = {
     
-    removeParkingHouse: removeParkingHouse,
-    returnArray: returnArray,
-    findPlace: findPlace,
-    addParkingHouse: addParkingHouse,
-    newArray: newArray
-};
+    
+    
+    
+    function findPlace(targetAdress, targetCity) {
+        
+        for (let i = 0; i < parkingHouseList.length; i++) {
+            
+            
+            if (targetAdress === parkingHouseList[i][0]
+                && targetCity === parkingHouseList[i][1]) {
+                    
+                    return true;
+                    
+                }
+                
+                
+            }
+        }
+        
+        
+        function newArray(newAdress, newCity, newLatitude, newLongtitude){
+            let newParkingHouseCheck = [newAdress, newCity, newLatitude, newLongtitude];
+            return newParkingHouseCheck
+        }
+        
+        function  returnArray(){
+            return parkingHouseList;
+        }
+
+
+        module.exports = {
+            
+            removeParkingHouse: removeParkingHouse,
+            returnArray: returnArray,
+            findPlace: findPlace,
+            addParkingHouse: addParkingHouse,
+            newArray: newArray
+        };
--- js/admin.test.js
+++ js/admin.test.js
@@ -8,20 +8,22 @@ test("Check if they find target location", ()=> {
     expect(admin.findPlace("BRA VEIEN 6a", "Halden")).toBeTruthy();
 });
 
+test("Check if they  cant find target location", ()=> {
+    expect(admin.findPlace("BRA 23EN 6a", "Sweden")).toBeFalsy();
+});
 
 
 
 
-
+/* NP! Will overwrite the memory of parking space list!
 
 test("checks if Newparkinghouselist is without the found one", () => {
     expect(admin.removeParkingHouse("BRA VEIEN 6a", "Halden")).toEqual([
-        ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 32131],
+        ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 321.31],
         ["Ant 23", "Moss", 423.231, 62.132]
     ]);
     
-});
-
+});*/
 
 test("Checks if new array imput is valid", ()=>{
     expect(admin.newArray(
@@ -29,20 +31,20 @@ test("Checks if new array imput is valid", ()=>{
         )).toEqual(["Jessheim24", "Voss", 232.32, 512.32]);
     })
     
-    
-    test("Check if i wrote return array test correctly", () => {
+    /*
+    test("Check if i wrote return array test correctly :O", () => {
         expect(admin.returnArray()).toEqual([
-            ["BRA VEIEN 6a", "Halden", 45.32, 32131],
-            ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 32131],
-            ["Ant 23", "Moss", 423.231, 62.132],
+            ["BRA VEIEN 6a", "Halden", 45.32, 321.31],
+            ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 321.31],
+            ["Ant 23", "Moss", 423.231, 62.132]
         ]);
     });
-    
+    */
     
     test("Check if i newhouse works", () => {
-        expect(admin.returnArray()).toEqual([
-            ["BRA VEIEN 6a", "Halden", 45.32, 32131],
-            ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 32131],
+        expect(admin.addParkingHouse("Jessheim 24", "Voss", 232.32, 512.32)).toEqual([
+            ["BRA VEIEN 6a", "Halden", 45.32, 321.31],
+            ["MOSSE VEIEN 53b", "Fredrikstad", 43.32, 321.31],
             ["Ant 23", "Moss", 423.231, 62.132],
             ["Jessheim 24", "Voss", 232.32, 512.32]
         ]);

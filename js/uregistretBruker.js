f0e12d05d8d7ddc52f8303d5ccaad78cd87469ec
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2020-10-21 04:24:22 +0200

    Finished alfgorythem :+1:
    I need to fix my sleep schdule...

--- js/uregistretBruker.js
+++ js/uregistretBruker.js
@@ -13,12 +13,37 @@ var parkingHouseList = [
 
 
 var parkingHouseReservationInfo = [
-    [1,1,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 42016969, "Slowpoke_Rodriguez"],
+    [1,1,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
     [1,2,"EasyPark", "", "", 000000000, "NaN"],
-    [1,3,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 42016969, "Slowpoke_Rodriguez"],
-    [1,4,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 42016969, "Slowpoke_Rodriguez"],
-    [1,5,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 42016969, "Slowpoke_Rodriguez"],
-    [1,6,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 42016969, "Slowpoke_Rodriguez"],
+    [1,3,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1,4,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1,5,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1,6,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+
+    [2,1,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [2,2,"NotSoEasyPark", "", "", 000000000, "NaN"],
+    [2,3,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
+    [2,4,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [2,5,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [2,6,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+
+
+    [3,1,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [3,2,"BadSpot", "", "", 000000000, "NaN"],
+    [3,3,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
+    [3,4,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [3,5,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [3,6,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+
+
+    [4,1,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [4,2,"HandiCapSpot", "", "", 000000000, "NaN"],
+    [4,3,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 68516969, "Robbie_Rotten"],
+    [4,4,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [4,5,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [4,6,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+
+    
 ]
 //Current format on ParkingHosueReservationList [[]] = 
 //parkinghosueId, parkingspotID, ownerCompanyUserName, date and time of begging of  reservation,, date time of ending of reservation bruker telefon nummer, brukersbrukernavn
@@ -127,22 +152,18 @@ function createNearbyList(returnList) {
         return;
     }
 
-    console.log(returnList.length);
-
-
     let city;
     let adress;
     let parkingId;
-    //Might needd to change format of parkingHouseList so we can have adress street street number!
+    /*Might needd to change format of parkingHouseList so we can have adress street street number!
     //Creates a drop down list to select from
     //Currrent format is [] = adress, city, longititude, lattitude, ownerCompanyUserName
-    // number of spots,  unike ID of parking house 
+    // number of spots,  unike ID of parking house */
 
     for (let i = 0; i < returnList.length; i++) {
         adress = returnList[i][0]
         city = returnList[i][1];
         parkingId = returnList[i][6]
-
 
         dropDownList = dropDownList + '<option value="'
             + parkingId + '">' + adress + " " + city +
@@ -162,24 +183,27 @@ function createNearbyList(returnList) {
 function createParkingHouseInfo() {
     let selectedHouseId = document.getElementById("parkingHouseListNearby").value;
     let showInfoList = [[]];
-    let nearByParkingSelected;
+    
 
     
     //Loops thru all ids and retures them if they match
     if (selectedHouseId == "all"){
+        alert("VALUE WAS all")
+    
+        console.log("SELECTED NEARBY" + nearByParkingList)
 
-        console.log("RETURN LIST TEST" + nearByParkingList)
+        if (nearByParkingList.length > 0) {
+           
 
-        nearByParkingSelected = nearByParkingList;
+            for (let i = 0; i < nearByParkingList.length; i++) {
 
-        if (!nearByParkingList == null) {
+                for (let m = 0; m < parkingHouseReservationInfo.length; m++){
 
-            for (let i = 0; i < nearByParkingSelected.length; i++) {
-
-                for (let m = 0; m < parkingHouseReservationInfo.length; i++){
-
-                    if (nearByParkingSelected[i][6] == parkingHouseReservationInfo[m][0])
-                        alert("INNER IF ")
+                    
+                    if (
+                        nearByParkingList[i][6]
+                         == parkingHouseReservationInfo[m][0])
+                      
                         showInfoList.push(parkingHouseReservationInfo[m])
                 }
             }
@@ -187,19 +211,33 @@ function createParkingHouseInfo() {
     }
     
 
-    
+    //loops tuor just one id 
     else{
-        
+        for (let m = 0; m < parkingHouseReservationInfo.length; m++){    
+            if (
+                selectedHouseId  == parkingHouseReservationInfo[m][0])
+              
+                showInfoList.push(parkingHouseReservationInfo[m])
+        }
     }
 
-console.log("PRE REMOVE INFOLIST \n" + showInfoList)
+    console.log("LENGHT IS \n" + showInfoList.length);
+
+    for(let i = 0; i <showInfoList.length; i ++){
+        console.log(showInfoList[i] +"\n")
+    }
 
     //Hides user info for unecasry eyes
     for(let i = 0; i<showInfoList.length; i ++){
-        showInfoList.pop()
-        showInfoList.pop()
+        showInfoList[i].pop()
+        showInfoList[i].pop()
     }
-    console.log("INFO FOUND\n " +showInfoList)
+
+    console.log("REMOVED INFO\n ")
+    for(let i = 0; i <showInfoList.length; i ++){
+        console.log(showInfoList[i] +"\n")
+    }
+
 }
 
 module.exports = {

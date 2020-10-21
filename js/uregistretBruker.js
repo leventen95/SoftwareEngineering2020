9a22c749167fd45534f0256231286d083a40dacc
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2020-10-21 10:58:27 +0200

    minor bug fix

--- js/uregistretBruker.js
+++ js/uregistretBruker.js
@@ -13,37 +13,37 @@ var parkingHouseList = [
 
 
 var parkingHouseReservationInfo = [
-    [1,1,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [1,2,"EasyPark", "", "", 12345678, "NaN"],
-    [1,3,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [1,4,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [1,5,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [1,6,"EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1, 1, "EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1, 2, "EasyPark", "", "", 12345678, "NaN"],
+    [1, 3, "EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1, 4, "EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1, 5, "EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [1, 6, "EasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
 
-    [2,1,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
-    [2,2,"NotSoEasyPark", "", "", 12345678, "NaN"],
-    [2,3,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
-    [2,4,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
-    [2,5,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [2,6,"NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-
-
-    [3,1,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
-    [3,2,"BadSpot", "", "", 12345678, "NaN"],
-    [3,3,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
-    [3,4,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
-    [3,5,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [3,6,"BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [2, 1, "NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [2, 2, "NotSoEasyPark", "", "", 12345678, "NaN"],
+    [2, 3, "NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
+    [2, 4, "NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [2, 5, "NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [2, 6, "NotSoEasyPark", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
 
 
-    [4,1,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
-    [4,2,"HandiCapSpot", "", "", 12345678, "NaN"],
-    [4,3,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 68516969, "Robbie_Rotten"],
-    [4,4,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
-    [4,5,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
-    [4,6,"HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [3, 1, "BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [3, 2, "BadSpot", "", "", 12345678, "NaN"],
+    [3, 3, "BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 68516969, "Robbie_Rotten"],
+    [3, 4, "BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [3, 5, "BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [3, 6, "BadSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
 
-    
+
+    [4, 1, "HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Speedy_Gonzales"],
+    [4, 2, "HandiCapSpot", "", "", 12345678, "NaN"],
+    [4, 3, "HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time", 68516969, "Robbie_Rotten"],
+    [4, 4, "HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 23401669, "Hipster_Jesus"],
+    [4, 5, "HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+    [4, 6, "HandiCapSpot", "Wed Oct 21 2020 03:34:08 GMT+0200 (Central European Summer Time", "Wed Oct 22 2020 03:34:08 GMT+0200 (Central European Summer Time)", 42016969, "Slowpoke_Rodriguez"],
+
+
 ]
 //Current format on ParkingHosueReservationList [[]] = 
 //parkinghosueId, parkingspotID, ownerCompanyUserName, date and time of begging of  reservation,, date time of ending of reservation bruker telefon nummer, brukersbrukernavn
@@ -183,70 +183,70 @@ function createNearbyList(returnList) {
 function createParkingHouseInfo() {
     let selectedHouseId = document.getElementById("parkingHouseListNearby").value;
     let showInfoList = [[]];
-    
 
-    
+
+
     //Loops thru all ids and retures them if they match
-    if (selectedHouseId == "all"){
-        alert("VALUE WAS all")
-    
+    if (selectedHouseId == "all") {
+        
         console.log("SELECTED NEARBY" + nearByParkingList)
-
+        
         if (nearByParkingList.length > 0) {
-           
-
+            
+            
             for (let i = 0; i < nearByParkingList.length; i++) {
-
-                for (let m = 0; m < parkingHouseReservationInfo.length; m++){
-
+                
+                for (let m = 0; m < parkingHouseReservationInfo.length; m++) {
+                    
                     
                     if (
                         nearByParkingList[i][6]
-                         == parkingHouseReservationInfo[m][0])
-                      
+                        == parkingHouseReservationInfo[m][0])
+                        
                         showInfoList.push(parkingHouseReservationInfo[m])
+                    }
                 }
             }
         }
-    }
-    
-
-    //loops tuor just one id 
-    else{
-        for (let m = 0; m < parkingHouseReservationInfo.length; m++){    
-            if (
-                selectedHouseId  == parkingHouseReservationInfo[m][0])
-              
-                showInfoList.push(parkingHouseReservationInfo[m])
-        }
-    }
-
-    console.log("LENGHT IS \n" + showInfoList.length);
-
-    for(let i = 0; i <showInfoList.length; i ++){
-        console.log(showInfoList[i] +"\n")
-    }
-
-    //Hides user info for unecasry eyes
-    for(let i = 0; i<showInfoList.length; i ++){
-        showInfoList[i].pop()
-        showInfoList[i].pop()
-    }
-
-
-    //SORTS BY SECOND COLLUM, THATS IS BY PARKINGSSPOT ID
-    showInfoList = showInfoList.sort(function(a,b) {
-        return a[1] - b[1];
-    });
-
-    //SORTS IT THEN BY FIRST COLLUM TO MAKE IT MORE READABLE
-    showInfoList = showInfoList.sort(function(a,b) {
+        
+        
+        //loops tuor just one id 
+        else {
+            for (let m = 0; m < parkingHouseReservationInfo.length; m++) {
+                if (
+                    selectedHouseId == parkingHouseReservationInfo[m][0])
+                    
+                    showInfoList.push(parkingHouseReservationInfo[m])
+                }
+            }
+            
+            console.log("LENGHT IS \n" + showInfoList.length);
+            
+            for (let i = 0; i < showInfoList.length; i++) {
+                console.log(showInfoList[i] + "\n")
+            }
+            
+            //Hides user info for unecasry eyes
+            for (let i = 0; i < showInfoList.length; i++) {
+                showInfoList[i].pop()
+                showInfoList[i].pop()
+            }
+            
+            
+            //SORTS BY SECOND COLLUM, THATS IS BY PARKINGSSPOT ID
+            showInfoList = showInfoList.sort(function (a, b) {
+                return a[1] - b[1];
+            });
+            
+            alert("VALUE WAS" + selectedHouseId)
+            //SORTS IT THEN BY FIRST COLLUM TO MAKE IT MORE READABLE
+    showInfoList = showInfoList.sort(function (a, b) {
         return a[0] - b[0];
     });
 
     console.log("REMOVED INFO\n ")
-    for(let i = 0; i <showInfoList.length; i ++){
-        console.log(showInfoList[i] +"\n")
+    for (let i = 0; i < showInfoList.length; i++) {
+        console.log(showInfoList[i] + "\n")
     }
 
 
